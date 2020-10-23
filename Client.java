c6fecb1017f83a8d608b9b63d76cdf2cbfcc29a6
Arnav Pawar <72827303+ArnavPawar@users.noreply.github.com>
2020-10-23 18:22:21 -0400

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,126 @@
+/* *
+ * Fract743ion lab with reduce
+ * 
+ * Arnav Pawar
+ * due 10/18/18
+ */
+public class Client
+{
+    public static void main(String[] args)
+    {
+        //initial
+        Fractions frac1 = new Fractions(1,2);
+        Fractions frac2 = new Fractions(3,4);
+        //den=0
+        Fractions frac3 = new Fractions(8,0);
+        Fractions frac4 = new Fractions(1,3);
+        //num=0
+        Fractions frac5 = new Fractions(0,5);
+        Fractions frac6 = new Fractions(1,5);
+        //den=1
+        Fractions frac7 = new Fractions(7,1);
+        Fractions frac8 = new Fractions(4,6);
+        
+        //2ED PART OF FRACTION LAB
+        //REDUCE
+        Fractions frac9 = new Fractions(12, 3);//when num>den
+        Fractions frac10 = new Fractions(10, 30);//when den>num
+        Fractions frac11= new Fractions(12000, 320000);//large #
+
+        
+        //EQUALS
+        Fractions frac12 = new Fractions(4,12);
+        Fractions frac13 = new Fractions(1, 3); 
+
+        Fractions frac14 = new Fractions(19,9);
+        Fractions frac15 = new Fractions(2,4); 
+        
+        Fractions sum;
+        Fractions subtract;
+        Fractions multiply;
+        Fractions divide;
+        Fractions reduce;
+        
+        //FRAC1-8 is normal
+        sum=frac1.add(frac2);
+        subtract = frac1.subtract(frac2);
+        multiply = frac1.mult(frac2);
+        divide = frac1.divide(frac2);       
+        System.out.println(frac1.toString() + " + " +frac2.toString() + " = " + sum.toString());
+        System.out.println(frac1.toString() + " - " +frac2.toString() + " = " + subtract.toString());
+        System.out.println(frac1.toString() + " * " +frac2.toString() + " = " + multiply.toString());
+        System.out.println(frac1.toString() + " / " +frac2.toString() + " = " + divide.toString());
+       
+        sum=frac3.add(frac4);
+        subtract = frac3.subtract(frac4);
+        multiply = frac3.mult(frac4);
+        divide = frac3.divide(frac4);        
+        System.out.println(frac3.toString() + " + " +frac4.toString() + " = " + sum.toString());
+        System.out.println(frac3.toString() + " - " +frac4.toString() + " = " + subtract.toString());
+        System.out.println(frac3.toString() + " * " +frac4.toString() + " = " + multiply.toString());
+        System.out.println(frac3.toString() + " / " +frac4.toString() + " = " + divide.toString());
+       
+        sum=frac5.add(frac6);
+        subtract = frac5.subtract(frac6);
+        multiply = frac5.mult(frac6);
+        divide = frac5.divide(frac6);        
+        System.out.println(frac5.toString() + " + " +frac6.toString() + " = " + sum.toString());
+        System.out.println(frac5.toString() + " - " +frac6.toString() + " = " + subtract.toString());
+        System.out.println(frac5.toString() + " * " +frac6.toString() + " = " + multiply.toString());
+        System.out.println(frac5.toString() + " / " +frac6.toString() + " = " + divide.toString());
+
+         
+        sum=frac7.add(frac8);
+        subtract = frac7.subtract(frac8);
+        multiply = frac7.mult(frac8);
+        divide = frac7.divide(frac8);
+        System.out.println(frac7.toString() + " + " +frac8.toString() + " = " + sum.toString());
+        System.out.println(frac7.toString() + " - " +frac8.toString() + " = " + subtract.toString());
+        System.out.println(frac7.toString() + " * " +frac8.toString() + " = " + multiply.toString());
+        System.out.println(frac7.toString() + " / " +frac8.toString() + " = " + divide.toString());
+        
+        //REDUCE METHOD
+        reduce = Fractions.reduce(frac9);
+        System.out.println("original is "+frac9.toString() + " and the reduced formation will be " + reduce.toString());
+        reduce = Fractions.reduce(frac10);
+        System.out.println("original is "+frac10.toString() + " and the reduced formation will be " + reduce.toString());
+        reduce = Fractions.reduce(frac11);
+        System.out.println("original is "+frac11.toString() + " and the reduced formation will be " + reduce.toString());
+       
+        //TESTS THE EQUAL METHOD
+        System.out.println(frac12.toString() + " and " + frac13.toString() + " is  " + frac12.equals(frac13));//TRUE
+        
+        System.out.println(frac14.toString() + " and " + frac15.toString() + " is  " + frac14.equals(frac15));//FALSE
+
+       }
+
+
+
+
+}
+
+//OUTPUTS
+/*
+ 1 / 2 + 3 / 4 = 10 / 8
+1 / 2 - 3 / 4 = 2 / 8
+1 / 2 * 3 / 4 = 3 / 8
+1 / 2 / 3 / 4 = 2 / 3
+fraction is undifined + 1 / 3 = fraction is undifined
+fraction is undifined - 1 / 3 = fraction is undifined
+fraction is undifined * 1 / 3 = fraction is undifined
+fraction is undifined / 1 / 3 = fraction is undifined
+0 + 1 / 5 = 5 / 25
+0 - 1 / 5 = 5 / 25
+0 * 1 / 5 = 0
+0 / 1 / 5 = 0
+7 + 4 / 6 = 46 / 6
+7 - 4 / 6 = 38 / 6
+7 * 4 / 6 = 14 / 3
+7 / 4 / 6 = 21 / 2
+original is 12 / 3 and the reduced formation will be 4
+original is 10 / 30 and the reduced formation will be 1 / 3
+original is 12000 / 320000 and the reduced formation will be 3 / 80
+4 / 12 and 1 / 3 is  true
+19 / 9 and 2 / 4 is  false
+ */
+
---
+++ Fractions.java
@@ -0,0 +1,155 @@
+/* *
+ * Fraction lab with reduce
+ * 
+ * Arnav Pawar
+ * due 10/18/18
+ */
+public class Fractions
+{
+  private int num;
+  private int den;
+    Fractions()
+    {
+        den=1;
+        num=0;
+    }
+    public Fractions(int num,int den)
+    {
+      this.num=num;
+      this.den=den;
+    }
+    public Fractions add(Fractions other)
+    { 
+        int num;
+        int den;
+        
+        den = other.den * this.den;
+        num = (this.num * other.den) + (other.num * this.den);
+    
+        if (num < 0)
+        {
+            num = num * -1;            
+        }
+        
+        Fractions add = new Fractions(num, den);
+        
+        add.reduce(num, den);//REDUCES THE FRAC
+
+        return this.reduce(num, den);
+    }
+    
+    public Fractions subtract(Fractions other)
+    {
+        int subNum;
+        int subDen;
+        
+        subNum = other.num * -1;
+        subDen = other.den;
+
+        Fractions frac = new Fractions(subNum, subDen);
+        return this.add(frac);
+    }
+    public Fractions mult(Fractions other)
+    {        
+        int Multnum;
+        int Multden;
+
+        Multnum = this.num * other.num;
+        Multden = this.den * other.den;
+        Fractions mult = new Fractions(Multnum, Multden);
+        
+        return this.reduce(mult);//Reduce method
+    }
+    public Fractions divide(Fractions other)
+    {
+        int Divnum;
+        int Divden;
+        Divnum = other.den;
+        Divden = other.num;
+        Fractions div = new Fractions(Divnum, Divden);
+        return this.mult(div);
+    } 
+    public Fractions reduce(int num, int den) 
+    {        
+        if (num < 0){
+            num = num * -1;            
+        }
+        
+        int gcd = 1;
+        int num1 = this.num;
+        int num2 = this.den;
+                
+        for (int a = 1; a <= num1 && a <= num2; a++)
+        {
+            if (num1 % a == 0 && num2 % a == 0)
+            {              
+            gcd = a;
+            }
+        }   
+        int FinalNum = num / gcd;
+        int FinalDen = den / gcd;
+        
+        Fractions frac = new Fractions(FinalNum,FinalDen);
+        return frac;
+     }
+
+     public static Fractions reduce(Fractions other) 
+     //STATIC REDUCE
+     {      
+        if (other.num < 0)
+        {
+            other.num = other.num * -1;            
+        }
+        
+        int gcd = 1;
+        int num1 = other.num;
+        int num2 = other.den;
+        
+        for (int a = 1; a <= num1 && a <= num2; a++) 
+        {
+            if (num1 % a == 0 && num2 % a == 0)
+                gcd = a;
+        }      
+        int Addnum = other.num / gcd;
+        int Addden = other.den / gcd;
+        
+        Fractions newfrac = new Fractions(Addnum, Addden);
+        return newfrac;
+    }    
+    public String toString()
+    {       
+        if(num==0)
+        {
+            return "0";
+        }
+        else if(den==0)
+        {
+            return"fraction is undifined";
+        }
+        else if(den==1)
+        {
+            return num+"";//Has to have quotes because it has to reurn a string
+        }
+        else
+        {
+            return num+ " / "+den;
+        }        
+    }
+    public boolean equals(Fractions other)
+    //EQUALS METHOD
+    {
+        Fractions f1 = new Fractions(num,den);
+        Fractions f2 = new Fractions(other.num,other.den);
+        
+        if(Fractions.reduce(f1).num==Fractions.reduce(f2).num)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}        
+   
+
